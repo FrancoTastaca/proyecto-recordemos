dc58c3547e67394dc0015be3f2cd39c707498c33
FrancoTastaca <franc0tastac@gmail.com>
2024-06-21 01:54:45 -0300

    Agrego la primer configuracion del back

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Middleware para analizar el cuerpo de las solicitudes
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+  });
