a68c049fcc2d74d5f3cb084de138a64aba1f3120
FrancoTastaca <franc0tastac@gmail.com>
2024-06-21 15:26:11 -0300

    Agrego ruta de prueba en index.js

--- backend/index.js
+++ backend/index.js
@@ -11,3 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
   });
+
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
