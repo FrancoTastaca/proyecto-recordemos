042e9670b5b1562f5425d06cb20d754376fc18f6
VriaH <vicky627243@gmail.com>
2024-07-27 05:28:31 -0300

    Change: se actualizan imagenes y se añade isLast en data

--- frontend/recuerdame_app/data.js
+++ frontend/recuerdame_app/data.js
@@ -2,16 +2,17 @@ export default [
     {
         id: '1',
         title: '¡BIENVENID@!',
-        image: require('./assets/favicon.png'),
+        image: require('./assets/Bienvenido.png'),
     },
     {
         id: '2',
         title: 'Configurá las alarmas',
-        image: require('./assets/favicon.png'),
+        image: require('./assets/Configurar.png'),
     },
     {
         id: '3',
         title: 'Controlá la medicación',
-        image: require('./assets/favicon.png'),
+        image: require('./assets/Controlar.png'),
+        isLast: true,
     },
 ];
