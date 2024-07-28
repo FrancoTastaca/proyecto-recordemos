c56cdcf4839398decd929d24c4a4ac564bf31276
VriaH <vicky627243@gmail.com>
2024-07-28 03:48:24 -0300

    Change: se rediseña Home y se redirecciona btn 'Soy cuidador'

--- frontend/recuerdame_app/components/HomeScreen.js
+++ frontend/recuerdame_app/components/HomeScreen.js
@@ -1,19 +1,78 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet, SafeAreaView, ImageBackground, Dimensions, TouchableOpacity } from "react-native";
+const { height } = Dimensions.get("window");
 
 function HomeScreen({ navigation }) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>HomeScreen</Text>
-        <Button
-          title="Soy cuidador"
-          onPress={() => navigation.navigate('SignIn')}
-        />
-        <Button
-          title="Soy paciente"
-          //onPress={() => navigation.navigate('Details')}
-        />
-      </View>
+      <SafeAreaView>
+        <View>
+          <View
+            style={{
+              paddingHorizontal: 40,
+              paddingTop: 100,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 35,
+                color: '#624D8A',
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}
+            >RECUERDAME</Text>
+            <ImageBackground
+              style={{
+                height: height / 2.5, 
+              }}
+              resizeMode="contain"
+              source={require("../assets/Home.png")}
+            />
+            <Text
+              style={{
+                fontSize: 20,
+                color: '#624D8A',
+                textAlign: 'left',
+                marginTop: 20,
+              }}
+            >Seleccione el perfil</Text>
+          </View>
+          <View style={{
+            paddingHorizontal: 20,
+            paddingVertical: 40,
+            alignItems: 'center',
+          }}>
+            <TouchableOpacity 
+              onPress={ () => navigation.navigate('SignIn')}
+              style={{
+                backgroundColor: '#624D8A',
+                paddingVertical: 15,
+                paddingHorizontal: 20,
+                width: '86%',
+                borderRadius: 8,
+                marginBottom: 14,
+              }}>
+              <Text style={{
+                color: '#f1eff8',
+                fontSize: 20,
+                textAlign: 'center',
+              }}>Soy cuidador</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{
+              backgroundColor: '#624D8A',
+              paddingVertical: 15,
+              paddingHorizontal: 20,
+              width: '86%',
+              borderRadius: 8,
+            }}>
+              <Text style={{
+                color: '#f1eff8',
+                fontSize: 20,
+                textAlign: 'center',
+              }}>Soy paciente</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     );
 }
 
