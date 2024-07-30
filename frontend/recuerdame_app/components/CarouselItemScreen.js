3a3eb7e3474d0b770b43c3250a71c77ff92bac58
VriaH <vicky627243@gmail.com>
2024-07-30 12:19:30 -0300

    Change: se cambia la estructura del carousel

--- frontend/recuerdame_app/components/CarouselItemScreen.js
+++ frontend/recuerdame_app/components/CarouselItemScreen.js
@@ -1,68 +1,64 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, useWindowDimensions, TouchableOpacity, Button } from 'react-native';
+import { View, Text, useWindowDimensions, TouchableOpacity, SafeAreaView, ImageBackground, Dimensions } from 'react-native';
 import { useNavigation } from "@react-navigation/native"
+const { height } = Dimensions.get("window");
 
 export default CarouselItem = ({ item }) => {
     const { width } = useWindowDimensions(); //Utiliza el valor del ancho de la pantalla
     const navigation = useNavigation();
     return(
-        <View style={[styles.container, { width }]}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>{item.title}</Text>
-            </View>
-            <Image source={item.image} style={styles.image} resizeMode="contain" />
-            { item.isLast && ( //Botón "Empezar" para el último ítem del Carousel y redirección al seleccionarlo
-                <View 
+        <SafeAreaView style={{flex: 1, backgroundColor: '#CECAE8', justifyContent: 'center'}}>
+            <View>
+                <View
                     style={{
-                        paddingHorizontal: 20,
-                        paddingVertical: 40,
-                        alignItems: 'center',
-                    }}>
-                    <TouchableOpacity 
-                      onPress={ () => navigation.navigate('Home')}
-                      style={{
-                        backgroundColor: '#624D8A',
-                        paddingVertical: 15,
-                        paddingHorizontal: 20,
-                        width: '48%',
-                        borderRadius: 8,
-                        marginBottom: 14,
-                      }}>
-                      <Text style={{
-                        color: '#f1eff8',
-                        fontSize: 22,
+                        width,
+                        paddingTop: 110,
+                    }}
+                >
+                    <Text
+                    style={{
+                        fontSize: 35,
+                        color: '#624D8A',
+                        fontWeight: 'bold',
                         textAlign: 'center',
-                      }}>Empezar</Text>
-                    </TouchableOpacity>
+                        marginBottom: 20,
+                    }}
+                    >{item.title}</Text>
+                    <ImageBackground
+                    style={{
+                        marginTop: 20,
+                        height: height / 2.5, 
+                    }}
+                    resizeMode="contain"
+                    source={item.image}
+                    />
                 </View>
-            )}
-        </View>
+                { item.isLast && (
+                    <View 
+                        style={{
+                            paddingHorizontal: 20,
+                            paddingVertical: 40,
+                            alignItems: 'center',
+                        }}>
+                        <TouchableOpacity 
+                            onPress={ () => navigation.navigate('Home')}
+                            style={{
+                                backgroundColor: '#624D8A',
+                                paddingVertical: 15,
+                                paddingHorizontal: 20,
+                                width: '86%',
+                                borderRadius: 8,
+                                marginBottom: 14,
+                            }}>
+                        <Text style={{
+                            color: '#f1eff8',
+                            fontSize: 22,
+                            textAlign: 'center',
+                        }}>Empezar</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            </View>
+        </SafeAreaView>        
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#CECAE8',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        fontWeight: '600',
-        fontSize: 32,
-        marginBottom: 10,
-        color: '#33294C',
-        textAlign: 'center',
-    },
-    titleContainer: {
-        flex: 0.3,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    image: {
-        flex: 0.5,
-        justifyContent: 'center',
-        width: '50%',
-        height: 100,
-        marginBottom: 50,
-    },
-});
