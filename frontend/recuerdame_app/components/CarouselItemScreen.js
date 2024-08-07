af3773d480ab7278eb1adb5ba579763fb0e5d767
VriaH <vicky627243@gmail.com>
2024-08-07 20:47:33 -0300

    Change: se cambia estructura del carousel y el tamaño de las imagenes

--- frontend/recuerdame_app/components/CarouselItemScreen.js
+++ frontend/recuerdame_app/components/CarouselItemScreen.js
@@ -7,58 +7,67 @@ export default CarouselItem = ({ item }) => {
     const { width } = useWindowDimensions(); //Utiliza el valor del ancho de la pantalla
     const navigation = useNavigation();
     return(
-        <SafeAreaView style={{flex: 1, backgroundColor: '#CECAE8', justifyContent: 'center'}}>
+        <SafeAreaView style={styles.container}>
             <View>
                 <View
                     style={{
                         width,
-                        paddingTop: 110,
+                        paddingTop: 80,
                     }}
                 >
                     <Text
-                    style={{
-                        fontSize: 35,
-                        color: '#624D8A',
-                        fontWeight: 'bold',
-                        textAlign: 'center',
-                        marginBottom: 20,
-                    }}
+                    style={styles.text}
                     >{item.title}</Text>
                     <ImageBackground
                     style={{
-                        marginTop: 20,
-                        height: height / 2.5, 
+                        marginTop: 50,
+                        marginBottom: 40,
+                        height: item.id === '2' ? height / 2.5 : height / 3, 
                     }}
                     resizeMode="contain"
                     source={item.image}
                     />
                 </View>
                 { item.isLast && (
-                    <View 
-                        style={{
-                            paddingHorizontal: 20,
-                            paddingVertical: 40,
-                            alignItems: 'center',
-                        }}>
+                    <View style={styles.containerTouch}>
                         <TouchableOpacity 
                             onPress={ () => navigation.navigate('Home')}
-                            style={{
-                                backgroundColor: '#624D8A',
-                                paddingVertical: 15,
-                                paddingHorizontal: 20,
-                                width: '86%',
-                                borderRadius: 8,
-                                marginBottom: 14,
-                            }}>
-                        <Text style={{
-                            color: '#f1eff8',
-                            fontSize: 22,
-                            textAlign: 'center',
-                        }}>Empezar</Text>
+                            style={styles.touch}>
+                        <Text style={styles.touchText}>Empezar</Text>
                         </TouchableOpacity>
                     </View>
                 )}
             </View>
         </SafeAreaView>        
     );
+}
+
+const styles = {
+    container: {
+        flex: 1, 
+        backgroundColor: '#CECAE8', 
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 50,
+        color: '#624D8A',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    containerTouch: {
+        paddingHorizontal: 20,
+        alignItems: 'center'
+    },
+    touch: {
+        backgroundColor: '#624D8A',
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        width: '86%',
+        borderRadius: 8
+    },
+    touchText: {
+        color: '#f1eff8',
+        fontSize: 22,
+        textAlign: 'center',
+    }
 }
