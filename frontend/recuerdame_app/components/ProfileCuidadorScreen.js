d4407387f5a7e8e34c4835811d8039075a71c789
VriaH <vicky627243@gmail.com>
2024-08-13 22:33:53 -0300

    Add: se añade pantalla del perfil Cuidador

--- frontend/recuerdame_app/components/ProfileCuidadorScreen.js
+++ frontend/recuerdame_app/components/ProfileCuidadorScreen.js
@@ -1,12 +1,71 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faDropbox } from '@fortawesome/free-brands-svg-icons';
+import { faSuitcaseMedical } from '@fortawesome/free-solid-svg-icons';
+import { faCalendarDays } from "@fortawesome/free-regular-svg-icons";
+import { faQrcode } from '@fortawesome/free-solid-svg-icons';
 
-function ProfileCuidadorScreen({}) {
+function ProfileCuidadorScreen() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#CECAE8', alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Perfil Cuidador</Text>
+      <View style={styles.container}>
+        <View style={styles.touchsWrapper}>
+          <TouchableOpacity style={styles.touchItem}>
+          <FontAwesomeIcon icon={faDropbox} size={40} style={styles.iconoItem} />  
+            <Text style={styles.textTouch}>Pastilleros</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.touchItem}>
+          <FontAwesomeIcon icon={faSuitcaseMedical} size={40} style={styles.iconoItem} />  
+            <Text style={styles.textTouch}>Medicamentos</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.touchItem}>
+            <FontAwesomeIcon icon={faCalendarDays} size={40} style={styles.iconoItem} />
+            <Text style={styles.textTouch}>Final del día</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.touchItem}>
+            <FontAwesomeIcon icon={faQrcode} size={40} style={styles.iconoItem} />
+            <Text style={styles.textTouch}>Código QR Paciente</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    backgroundColor: '#CECAE8', 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  touchsWrapper: {
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  touchItem: {
+    backgroundColor: '#624D8A',
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    width: 400,
+    height: 100,
+    borderRadius: 8,
+    marginBottom: 30,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  iconoItem: {
+    alignSelf: 'left',
+    //marginRight: 10,
+    color: '#CECAE8'
+  },   
+  textTouch:{
+    color: '#f1eff8',
+    fontSize: 24,
+    textAlign: 'center',
+    flex: 1
+  }
+})
 
 export default ProfileCuidadorScreen;
