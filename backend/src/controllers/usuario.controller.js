0a8dc46662fbe484fd500ada761557a22ab7b5c9
FrancoTastaca <franc0tastac@gmail.com>
2024-09-03 16:58:34 -0300

    fix(user): Agregar funcionalidad de getRole como endpoint para el usuario logueado

--- backend/src/controllers/usuario.controller.js
+++ backend/src/controllers/usuario.controller.js
@@ -86,29 +86,33 @@ export default {
 
   getRole: async (req, res) => {
     try {
-      const usuario = await models.Usuario.findByPk(req.params.id);
-      if (!usuario) {
-        return res.status(errors.UsuarioNoEncontrado.code).json({
-          success: false,
-          message: errors.UsuarioNoEncontrado.message
+        const personaID = res.locals.usuario.persona.ID;
+        const persona = await models.Persona.findByPk(personaID, {
+            attributes: ['tipo']
         });
-      }
-      const personaId = usuario.Persona_ID;
-      const cuidador = await models.Cuidador.findByPersonaId(personaId);
-      if (cuidador) {
-        return res.json({ success: true, role: 'Cuidador' });
-      }
-      const paciente = await models.Paciente.findByPersonaId(personaId);
-      if (paciente) {
-        return res.json({ success: true, role: 'Paciente' });
-      }
-      res.json({ success: true, role: 'Desconocido' });
+
+        if (!persona) {
+            return res.status(404).json({ success: false, message: 'Persona no encontrada' });
+        }
+
+        let role;
+        switch (persona.tipo) {
+            case 'C':
+                role = 'Cuidador';
+                break;
+            case 'P':
+                role = 'Paciente';
+                break;
+            default:
+                role = 'Desconocido';
+        }
+
+        res.json({ success: true, role });
     } catch (error) {
-      res.status(errors.InternalServerError.code).json({
-        success: false,
-        message: errors.InternalServerError.message
-      });
+        res.status(errors.InternalServerError.code).json({
+            success: false,
+            message: errors.InternalServerError.message
+        });
     }
   }
-  
 }
--- backend/src/routes/usuarioRoutes.js
+++ backend/src/routes/usuarioRoutes.js
@@ -7,9 +7,9 @@ import { checkRoleCuidador } from '../middlewares/checkRole.js';
 const router = express.Router()
 
 router.get('/', usuarioController.listar);
+router.get('/getRole', usuarioController.getRole);
 // Rutas CRUD de usuario por ID
 router.get('/:id', validate(usuarioScheme.read), checkRoleCuidador, usuarioController.readUsuario);
 router.put('/:id', validate(usuarioScheme.update), checkRoleCuidador, usuarioController.updateUsuario);
 router.delete('/:id', validate(usuarioScheme.remove), checkRoleCuidador, usuarioController.deleteUsuario);
-
 export default router;
