dadee30dc1cebe42cea8c487a73cef85edcf4aef
FrancoTastaca <franc0tastac@gmail.com>
2024-10-27 21:28:27 -0300

    fix(frontend): Corregir URL base de la API en axiosConfig.js

--- frontend/recuerdame_app/shared/axiosConfig.js
+++ frontend/recuerdame_app/shared/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.109:3000/api', // Cambia esto a la URL de tu API
+    baseURL: 'http://192.168.1.100:3000/api', // Cambia esto a la URL de tu API
     timeout: 5000,
     headers: { 'Content-Type': 'application/json' }
 })
